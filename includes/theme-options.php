aeb8f0efbedb3a30eb25c679ae1af2f8c3252829
James Koster <james@jameskoster.co.uk>
2013-03-03 16:37:59 +0000

    0.3

--- includes/theme-options.php
+++ includes/theme-options.php
@@ -15,61 +15,104 @@ class NimbusOptions
      * @see add_action('customize_register',$func)
      * @param WP_Customize_Manager $wp_customize
      */
-    public static function register( $wp_customize )
+    public static function nimbus_register( $wp_customize )
     {
-        $wp_customize->add_setting('link_textcolor', array(
-                'default'   => '#49a0a0',
-        ) );
-        $wp_customize->add_setting('headercolor', array(
-                'default'   => '#aaaaaa',
-        ) );
-        $wp_customize->add_setting('textcolor', array(
-                'default'   => '#666666',
-        ) );
-        $wp_customize->add_setting('background_color', array(
-                'default'   => '#2b2b2b',
-        ) );
-        $wp_customize->remove_section('static_front_page');
-        $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'link_textcolor', array(
-                'label'      => __( 'Link Color', 'nimbus' ),
-                'section'    => 'colors',
-                'settings'   => 'link_textcolor',
-        ) ) );
-        $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'headercolor', array(
-                'label'      => __( 'Header Color', 'nimbus' ),
-                'section'    => 'colors',
-                'settings'   => 'headercolor',
-        ) ) );
-        $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'textcolor', array(
-                'label'      => __( 'Text Color', 'nimbus' ),
-                'section'    => 'colors',
-                'settings'   => 'textcolor',
-        ) ) );
-        $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'background_color', array(
-                'label'      => __( 'Background Color', 'nimbus' ),
-                'section'    => 'colors',
-                'settings'   => 'background_color',
-        ) ) );
-
-
         /**
-         * Layout
+         * Theme Option Defaults
          */
-        // Add layout section
-        $wp_customize->add_section( 'nimbus_layout', array(
-            'title'    => __( 'Layout', 'nimbus' ),
-            'priority' => 50,
-        ) );
 
-
-        // Add layout default
+        // Layout Default
         $wp_customize->add_setting( 'nimbus_theme_options[theme_layout]', array(
             'type'              => 'option',
             'default'           => 'sidebar-content',
             'sanitize_callback' => 'sanitize_key',
         ) );
 
-        // Add layout options
+        // Typography Default
+        $wp_customize->add_setting( 'nimbus_theme_options[theme_typography]', array(
+            'type'              => 'option',
+            'default'           => 'open-sans',
+            'sanitize_callback' => 'sanitize_key',
+        ) );
+
+        // Link Color Default
+        $wp_customize->add_setting('link_textcolor', array(
+                'default'   => '#49a0a0',
+        ) );
+
+        // Heading Color Default
+        $wp_customize->add_setting('headercolor', array(
+                'default'   => '#aaaaaa',
+        ) );
+
+        // Text Color Default
+        $wp_customize->add_setting('textcolor', array(
+                'default'   => '#666666',
+        ) );
+
+        // Background Color Default
+        $wp_customize->add_setting('background_color', array(
+                'default'   => '#2b2b2b',
+        ) );
+
+
+        /**
+         * Theme Option Sections
+         */
+
+        // Navigation Section
+        $wp_customize->add_section( 'nav', array(
+             'title'          => __( 'Navigation' ),
+             'theme_supports' => 'menus',
+             'priority'       => 100,
+        ) );
+
+        // Layout Section
+        $wp_customize->add_section( 'nimbus_layout', array(
+            'title'    => __( 'Layout', 'nimbus' ),
+            'priority' => 50,
+        ) );
+
+        // Typography Section
+        $wp_customize->add_section( 'nimbus_typography', array(
+            'title'    => __( 'Typography', 'nimbus' ),
+            'priority' => 60,
+        ) );
+
+
+        /**
+         * Theme Option Controls
+         */
+
+        // Link Color Control
+        $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'link_textcolor', array(
+                'label'      => __( 'Link Color', 'nimbus' ),
+                'section'    => 'colors',
+                'settings'   => 'link_textcolor',
+        ) ) );
+
+        // Heading Color Control
+        $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'headercolor', array(
+                'label'      => __( 'Header Color', 'nimbus' ),
+                'section'    => 'colors',
+                'settings'   => 'headercolor',
+        ) ) );
+
+        // Text Color Control
+        $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'textcolor', array(
+                'label'      => __( 'Text Color', 'nimbus' ),
+                'section'    => 'colors',
+                'settings'   => 'textcolor',
+        ) ) );
+
+        // Background Color Control
+        $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'background_color', array(
+                'label'      => __( 'Background Color', 'nimbus' ),
+                'section'    => 'colors',
+                'settings'   => 'background_color',
+        ) ) );
+
+        // Layout Control
         $layouts = nimbus_layouts();
         $choices = array();
         foreach ( $layouts as $layout ) {
@@ -81,8 +124,19 @@ class NimbusOptions
             'choices'    => $choices,
         ) );
 
-
+        // Typography Control
+        $typography_options = nimbus_typography();
+        $choices = array();
+        foreach ( $typography_options as $typography_option ) {
+            $choices[$typography_option['value']] = $typography_option['label'];
+        }
+        $wp_customize->add_control( 'nimbus_theme_options[theme_typography]', array(
+            'section'    => 'nimbus_typography',
+            'type'       => 'radio',
+            'choices'    => $choices,
+        ) );
     }
+
 
     /**
      * This will output the custom WordPress settings to the theme's WP head.
@@ -91,7 +145,7 @@ class NimbusOptions
      *
      * @see add_action('wp_head',$func)
      */
-    public static function render()
+    public static function nimbus_render()
     {
         ?>
         <!--Customizer CSS-->
@@ -153,7 +207,7 @@ class NimbusOptions
      * @param string $postfix Optional. Anything that needs to be output after the CSS property
      * @param bool $echo Optional. Whether to print directly to the page (default: true).
      * @return string Returns a single line of CSS with selectors and a property.
-     * @since Nouveau 1.0
+     * @since Nimbus 0.1.1
      */
     public static function generate_css($selector,$style,$mod_name,$prefix='',$postfix='',$echo=true)
     {
@@ -175,8 +229,8 @@ class NimbusOptions
     }
 
 }
-add_action( 'customize_register'    , array( 'NimbusOptions' , 'register' ) );
-add_action( 'wp_head'               , array( 'NimbusOptions' , 'render' ) );
+add_action( 'customize_register'    , array( 'NimbusOptions' , 'nimbus_register' ) );
+add_action( 'wp_head'               , array( 'NimbusOptions' , 'nimbus_render' ) );
 
 $args = array(
     'default-color' => '2b2b2b',
@@ -187,7 +241,7 @@ add_theme_support( 'custom-background', $args );
 /**
  * Returns the options array for Nimbus.
  *
- * @since Nimbus 0.2.4
+ * @since Nimbus 0.3
  */
 function nimbus_get_theme_options() {
     return get_option( 'nimbus_theme_options', nimbus_get_default_theme_options() );
@@ -197,11 +251,12 @@ function nimbus_get_theme_options() {
 /**
  * Returns the default options for Nimbus.
  *
- * @since Nimbus 0.2.4
+ * @since Nimbus 0.3
  */
 function nimbus_get_default_theme_options() {
     $default_theme_options = array(
-        'theme_layout' => 'sidebar-content',
+        'theme_layout'      => 'sidebar-content',
+        'theme_typography'  => 'open-sans'
     );
 
     if ( is_rtl() )
@@ -214,7 +269,7 @@ function nimbus_get_default_theme_options() {
 /**
  * Returns an array of layout options registered for Nimbus.
  *
- * @since Nimbus 0.2.4
+ * @since Nimbus 0.3
  */
 function nimbus_layouts() {
     $layout_options = array(
@@ -241,7 +296,7 @@ function nimbus_layouts() {
 /**
  * Adds Nimbus layout classes to the array of body classes.
  *
- * @since Nimbus 0.2.4
+ * @since Nimbus 0.3
  */
 function nimbus_layout_classes( $existing_classes ) {
     $options = nimbus_get_theme_options();
@@ -264,3 +319,52 @@ function nimbus_layout_classes( $existing_classes ) {
     return array_merge( $existing_classes, $classes );
 }
 add_filter( 'body_class', 'nimbus_layout_classes' );
+
+
+/**
+ * Returns an array of typography options registered for Nimbus.
+ *
+ * @since Nimbus 0.3
+ */
+function nimbus_typography() {
+    $typography_options = array(
+        'open-sans' => array(
+            'value' => 'open-sans',
+            'label' => __( 'Open Sans', 'nimbus' ),
+        ),
+        'helvetica' => array(
+            'value' => 'helvetica',
+            'label' => __( 'Helvetica', 'nimbus' ),
+        ),
+        'georgia' => array(
+            'value' => 'georgia',
+            'label' => __( 'Georgia', 'nimbus' ),
+        ),
+    );
+
+    return apply_filters( 'nimbus_typography', $typography_options );
+}
+
+/**
+ * Adds Nimbus typography classes to the array of body classes.
+ *
+ * @since Nimbus 0.3
+ */
+function nimbus_typography_classes( $existing_classes ) {
+    $options = nimbus_get_theme_options();
+    $current_typography = $options['theme_typography'];
+
+    if ( 'open-sans' == $current_typography )
+        $classes[] = 'open-sans';
+    elseif ( 'helvetica' == $current_typography )
+        $classes[] = 'helvetica';
+    elseif ( 'georgia' == $current_typography )
+        $classes[] = 'georgia';
+    else
+        $classes[] = $current_typography;
+
+    $classes = apply_filters( 'nimbus_typography_classes', $classes, $current_typography );
+
+    return array_merge( $existing_classes, $classes );
+}
+add_filter( 'body_class', 'nimbus_typography_classes' );
